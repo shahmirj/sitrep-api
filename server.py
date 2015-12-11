210e485beb1787657bf276d37bddff5df7992501
Shahmir Javaid <shahmirj@gmail.com>
2015-12-11 15:41:37 +0000

    Adding favicon and static serve files

--- server.py
+++ server.py
@@ -7,14 +7,22 @@ import sys, os
 sys.path.append("./app");
 
 # Global includes
-from flask import Flask, Response
+from flask import Flask, Response, url_for
 from flask_restful import Resource, Api
 
 # App includes
 from controllers.IndexController import IndexController
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/pub')
 api = Api(app)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+      os.path.join(app.root_path, 'static'),
+      'favicon.ico',
+      mimetype='image/vnd.microsoft.icon'
+    )
 
 # Add the resources for routing
 api.add_resource(IndexController, "/")
